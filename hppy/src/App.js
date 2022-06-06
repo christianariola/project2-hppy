c0ba06ad55099ea934246854a5891ccaf5040d1d
otakuchan <ianbjs@gmail.com>
2022-06-06 01:37:22 -0700

    edit app.js

--- hppy/src/App.js
+++ hppy/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is from the stage branch
         </a>
       </header>
     </div>
