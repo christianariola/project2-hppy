9d8fd980c49c3e55aea8e255697434368d88a562
otakuchan <ianbjs@gmail.com>
2022-06-06 11:20:30 -0700

    update App.js

--- hppy/src/App.js
+++ hppy/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hppy.
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
