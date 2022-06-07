666e3f61f7aa53d04b4e0349e6886f61975e3394
otakuchan <ianbjs@gmail.com>
2022-06-07 16:18:47 -0700

    edit App.js to show this is from the dev branch

--- hppy/src/App.js
+++ hppy/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hppy.
+          Hppy. This is from the dev branch
         </p>
         <a
           className="App-link"
