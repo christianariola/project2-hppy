9eebb33edf4041df9ec3722cca93673d90ce3ddb
otakuchan <ianbjs@gmail.com>
2022-06-09 02:44:15 -0700

    setup express server with index endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,11 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
 
+const app = express()
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) 
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Hppy')
+})
