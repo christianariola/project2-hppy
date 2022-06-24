d9452c06e6ccfd9bdd5680502a4d1e7e3b63f43f
Karan <ksingh090@mylangara.ca>
2022-06-23 19:46:05 -0700

    updated modal

--- backend/models/dailySurveyModel.js
+++ backend/models/dailySurveyModel.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 
-const dailySurveySchema = mongoose.Schema(
-  {
-    questionOne: {
-      type: Number,
-      required: [true, "Please select a rating"],
-    },
-    questionTwo: {
-      type: String,
-      required: false,
-    },
+const dailySurveySchema = mongoose.Schema({
+  questionOne: {
+    type: Number,
+    required: [true, "Please select a rating"],
   },
-  {
-    timestamps: true,
-  }
-);
+  questionTwo: {
+    type: String,
+    required: false,
+  },
+  surveyState: {
+    type: String,
+    required: true,
+    default: "pending",
+  },
+  timestamps: true,
+});
 module.exports = mongoose.model("DailySurvey", dailySurveySchema);
