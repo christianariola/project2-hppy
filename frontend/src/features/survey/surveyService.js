6c7b742aacff2d32e8cb4dad735e32e75c092e3a
otakuchan <ianbjs@gmail.com>
2022-07-08 23:33:47 -0700

    change API_URL on feature/services for live deployment

--- frontend/src/features/survey/surveyService.js
+++ frontend/src/features/survey/surveyService.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // Local API_URL for dev
-const API_URL = '/api/survey'
-// const API_URL = "https://pluto-hppy.herokuapp.com/api/dailySurvey";
+// const API_URL = '/api/survey'
+const API_URL = "https://pluto-hppy.herokuapp.com/api/survey";
 
 const dailySurveyChecker = async (userData) => {
   const response = await axios.post(API_URL + '/dailychecker', userData)
