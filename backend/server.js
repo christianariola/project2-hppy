2e10bf58761a56365d976ec49b0a7ddb2932b6be
otakuchan <ianbjs@gmail.com>
2022-07-11 18:49:55 -0700

    Add report route and controller

--- backend/server.js
+++ backend/server.js
@@ -97,6 +97,7 @@ app.post("/weeklysurveys", (req, res) => {
 app.use("/api/employees", require("./routes/employeeRoutes"));
 app.use("/api/survey", require("./routes/surveyRoutes"))
 app.use("/api/companies", require("./routes/companyRoutes"))
+app.use("/api/reports", require("./routes/reportRoutes"))
 
 app.use("/api/dailySurvey", require("./routes/surveyRoutes"));
 app.use("/api/Surveys", require("./routes/surveyRoutes"));
