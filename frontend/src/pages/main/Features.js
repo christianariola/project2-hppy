628dc909d81d21946959d943bf198d978bc2d5ca
Karan <ksingh090@mylangara.ca>
2022-07-31 03:03:08 -0700

    Navbar styling

--- frontend/src/pages/main/Features.js
+++ frontend/src/pages/main/Features.js
@@ -1,50 +1,81 @@
-
 const Features = () => {
-    return <>
-        <div className="wrapper">
-            <h2>With Hppy, employers can now create working environment with employees feedback and build a culture where people flourish.</h2>
-            <section className="feature">
-                <div className="feature-1">
-                    <h3>Get Insights From Employees</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, illo eligendi! Fuga praesentium corporis facere quisquam officiis, blanditiis ducimus cum nam excepturi, repellendus totam suscipit veritatis! Sunt vero odio quidem.</p>
-                </div>
-                <div>
-                    <div className="hero-image"><img src="/images/get-insights.png" alt="Hppy" /></div>
-                </div>
-            </section>
+  return (
+    <>
+      <div className="wrapper">
+        <h2>
+          With Hppy, employers can now create working environment with employees
+          feedback and build a culture where people flourish.
+        </h2>
+        <section className="feature">
+          <div>
+            <div className="hero-image">
+              <img src="/images/get-insights.png" alt="Hppy" />
+            </div>
+          </div>
+          <div className="feature-1">
+            <h3>Get Insights From Employees</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi,
+              illo eligendi! Fuga praesentium corporis facere quisquam officiis,
+              blanditiis ducimus cum nam excepturi, repellendus totam suscipit
+              veritatis! Sunt vero odio quidem.
+            </p>
+          </div>
+        </section>
 
-            <section className="feature">
-                <div>
-                    <div className="hero-image"><img src="/images/generate-reports.png" alt="Hppy" /></div>
-                </div>
-                <div className="feature-2">
-                    <h3>Generate Reports</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, illo eligendi! Fuga praesentium corporis facere quisquam officiis, blanditiis ducimus cum nam excepturi, repellendus totam suscipit veritatis! Sunt vero odio quidem.</p>
-                </div>
-            </section>
+        <section className="feature">
+          <div>
+            <div className="hero-image">
+              <img src="/images/generate-reports.png" alt="Hppy" />
+            </div>
+          </div>
+          <div className="feature-2">
+            <h3>Generate Reports</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi,
+              illo eligendi! Fuga praesentium corporis facere quisquam officiis,
+              blanditiis ducimus cum nam excepturi, repellendus totam suscipit
+              veritatis! Sunt vero odio quidem.
+            </p>
+          </div>
+        </section>
 
-            <section className="feature">
-                <div className="feature-3">
-                    <h3>Anonymous survey responses</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, illo eligendi! Fuga praesentium corporis facere quisquam officiis, blanditiis ducimus cum nam excepturi, repellendus totam suscipit veritatis! Sunt vero odio quidem.</p>
-                </div>
-                <div>
-                    <div className="hero-image"><img src="/images/anonymous.png" alt="Hppy" /></div>
-                </div>
-            </section>
+        <section className="feature">
+          <div>
+            <div className="hero-image">
+              <img src="/images/anonymous.png" alt="Hppy" />
+            </div>
+          </div>
+          <div className="feature-3">
+            <h3>Anonymous survey responses</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi,
+              illo eligendi! Fuga praesentium corporis facere quisquam officiis,
+              blanditiis ducimus cum nam excepturi, repellendus totam suscipit
+              veritatis! Sunt vero odio quidem.
+            </p>
+          </div>
+        </section>
 
-
-            <section className="feature">
-                <div>
-                    <div className="hero-image"><img src="/images/sentiment-analysis.png" alt="Hppy" /></div>
-                </div>
-                <div className="feature-4">
-                    <h3>Text sentiment analysis base on AI</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, illo eligendi! Fuga praesentium corporis facere quisquam officiis, blanditiis ducimus cum nam excepturi, repellendus totam suscipit veritatis! Sunt vero odio quidem.</p>
-                </div>
-            </section>
-        </div>
+        <section className="feature">
+          <div>
+            <div className="hero-image">
+              <img src="/images/sentiment-analysis.png" alt="Hppy" />
+            </div>
+          </div>
+          <div className="feature-4">
+            <h3>Text sentiment analysis base on AI</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi,
+              illo eligendi! Fuga praesentium corporis facere quisquam officiis,
+              blanditiis ducimus cum nam excepturi, repellendus totam suscipit
+              veritatis! Sunt vero odio quidem.
+            </p>
+          </div>
+        </section>
+      </div>
     </>
-}
+  );
+};
 
-export default Features
+export default Features;
