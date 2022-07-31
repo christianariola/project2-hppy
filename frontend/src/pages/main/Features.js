b6168ccc346c73228b01897226663bac3cb3ff92
Karan <ksingh090@mylangara.ca>
2022-07-31 04:22:41 -0700

    landingpage styling

--- frontend/src/pages/main/Features.js
+++ frontend/src/pages/main/Features.js
@@ -3,8 +3,9 @@ const Features = () => {
     <>
       <div className="wrapper">
         <h2>
-          With Hppy, employers can now create working environment with employees
-          feedback and build a culture where people flourish.
+          <span className="powerWord">With Hppy</span>, employers can now create
+          working environment with employees feedback and build a culture where
+          people <span className="powerWord">flourish</span>.
         </h2>
         <section className="feature">
           <div>
@@ -73,6 +74,26 @@ const Features = () => {
             </p>
           </div>
         </section>
+        <div className="hero">
+          <div className="hero-text">
+            <h2>Improved employee engagement begins here.</h2>
+            <a href="#!">Contact Us</a>
+          </div>
+          <div className="hero-image">
+            <img src="/images/middle.png" alt="Hppy" />
+          </div>
+        </div>
+        <div className="feature-5">
+          <h3>
+            <span className="powerWord">Hppy</span> at a glance
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, illo
+            eligendi! Fuga praesentium corporis facere quisquam officiis,
+            blanditiis ducimus cum nam excepturi, repellendus totam suscipit
+            veritatis! Sunt vero odio quidem.
+          </p>
+        </div>
       </div>
     </>
   );
