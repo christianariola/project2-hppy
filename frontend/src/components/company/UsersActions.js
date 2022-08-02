75c65753b109f140d18b94f2ccb81a318d417080
otakuchan <ianbjs@gmail.com>
2022-08-02 03:27:27 -0700

    Company table layout

---
+++ frontend/src/components/company/UsersActions.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@mui/material/Link';
+
+const UsersActions = (params) => {
+    // console.log(params)
+    return (
+    <div>
+    <Link component={RouterLink} to={`/app/company/${params.row._id}`} variant="button" sx={{ my: 1, mx: 1.5 }}>
+    <IconButton aria-label="view">
+        <VisibilityIcon />
+    </IconButton>
+    </Link>
+
+    <Link component={RouterLink} to={`/app/company/edit/${params.row._id}`} variant="button" sx={{ my: 1, mx: 1.5 }}>
+    <IconButton aria-label="edit">
+        <ModeEditIcon />
+    </IconButton>
+    </Link>
+
+    <IconButton aria-label="delete" onClick={params.handleDelete}>
+        <DeleteIcon />
+    </IconButton>
+    </div>
+    )
+}
+
+export default UsersActions
